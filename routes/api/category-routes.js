b8b1407369bda437e05fbc76b2e25e0858e8ce6a
Marius Du Plooy <woestyn101@gmail.com>
2024-02-13 18:20:19 -0500

    post, update, delete added to category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -29,14 +29,47 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: 'Pants',
+
+  })
+  .then((new_cat) => {
+    // Sends the updated book as a json response
+    res.json(new_cat);
+  })
+  .catch((err) => res.json(err));
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update({
+    category_name: 'LongPants',
+
+  }, 
+  {
+    // Gets the books based on the isbn given in the request parameters
+    where: {
+        id: req.params.id,
+    },
+  })
+  .then((new_cat) => {
+    // Sends the updated book as a json response
+    res.json(new_cat);
+  })
+  .catch((err) => res.json(err));
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((deletedCategory) => {
+      res.json(deletedCategory);
+    })
+    .catch((err) => res.json(err));
 });
 
 module.exports = router;
